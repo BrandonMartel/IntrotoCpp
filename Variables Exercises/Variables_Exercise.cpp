ea04fa049670ade9cf5f4c3d4d694f781985c42b
BrandonMartel <martelbrandon99@gmail.com>
2017-08-14 17:19:47 -0500

    Class Work Assignment
    Variables Exercise

---
+++ Variables Exercises/Variables_Exercise.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+void main()
+{
+	//You can not print to the console unless the problem specifies.
+
+	//1. Select suitable variable types and names for the following examples
+	//a) The average height of students in your class
+	float avgStudentHeight_Programming1 = 66.0f;
+	//b) The tax rate for incomes over $65000
+	float taxRate_Over = 65000;
+	//c) The tax rate for incomes below $5000
+	float taxRate_Under = 5000;
+	//d) The total cost of all the games on your Steam library
+	int steamLibraryCost = 100;
+
+
+	//2. Select suitable data types to hold the following values :
+	//a) 121
+	int number;
+	//b) 9.4
+	float decimal;
+	//c) r
+	char letter;
+	//d) 100000
+	int largeNumber;
+	//e) False
+	bool trueFalse;
+
+	//3. Create a program that swaps two numbers. Ask the user for 2 numbers in the console window
+	//and store the result in variables called “a” and “b” print the result to screen.Write code 
+	//that swaps the values for these variables, then print the variables “a” and “b” again.
+	//You should comment your code.
+
+
+	//4. Ask the user for 5 numbers and output the average of these numbers.		
+
+	//5. Redo question 3 but allows the user to input the values to be swapped and provide information
+	//in the console proving the data was swapped.
+}
---
+++ Variables/Source.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+{
+	//class example
+	//INT data type
+	int myNumber = 9;
+	//floating point data types
+	float decimal = 0.5f;
+	//character data type
+	char myLetter = 'd';
+	//Boolean data type
+	bool trueFalse = false;
+
+	//Console read and write
+	system("pause");
+}
