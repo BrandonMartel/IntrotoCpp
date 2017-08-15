60db80df5be49fb086ec8a2382eaa456f31ab13c
BrandonMartel <martelbrandon99@gmail.com>
2017-08-15 16:03:41 -0500

    Loops Assignment

--- Variables Exercises/Conditional Assignment.cpp
+++ Variables Exercises/Conditional Assignment.cpp
@@ -18,10 +18,11 @@ int main()
 
 	//2. Write an if statement that assigns 100 to the value of x when y is equal to zero
 	
-	bool condition = true
+	int x, y;
+
 	if (y == 0)
 	{
-		100 = x
+		x = 100;
 	}
 
 
@@ -30,8 +31,57 @@ int main()
 	//It should then display the number of days in that month. If a number that doesn’t correspond 
 	//to a month is entered then the program should display an error message. You must use a switch statement
 
+	int a = 1;
+	switch (a)
+	{
+	case 1:
+		a == 31;
+		std::cout << "January has 31 days";
+		break;
+	case 2:
+		std::cout << "Febuary has 28 days";
+		break;
+	case 3:
+		std::cout << "March has 31 days";
+		break;
+	case 4:
+		std::cout << "April has 30 days";
+		break;
+	case 5:
+		std::cout << "May has 30 days";
+		break;
+	case 6:
+		std::cout << "June has 30 days";
+		break;
+	case 7:
+		std::cout << "July has 31 days";
+		break;
+	case 8:
+		std::cout << "August has 31 days";
+		break;
+	case 9:
+		std::cout << "September has 30 days";
+		break;
+	case 10:
+		std::cout << "October has 31 days";
+		break;
+	case 11:
+		std::cout << "November has 30 days";
+		break;
+	case 12:
+		std::cout << "December has 31 days";
+		break;
+
+	}
+
 
 	//4. Redo problem 3 using if statements
+
+
+
+
+
+
 
 
 	//5. Convert problem 2 into a ternary operator
