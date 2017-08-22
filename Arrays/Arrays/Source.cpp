f4c4246eac06cc1d46dc883ab37fb40ed23c9cdc
BrandonMartel <martelbrandon99@gmail.com>
2017-08-22 15:48:31 -0500

    arrays

---
+++ Arrays/Arrays/Source.cpp
@@ -0,0 +1,46 @@
+//1. Declare an array for each of the following(size of array does not matter unless
+//sepcified).
+//a. heights of students
+//int studentHeight[60];
+
+//b. age of parents
+//int parentAge[45];
+
+//c. true of false questions
+// bool trueFalseQuestions[5];
+
+//d. letters of the alphabet
+//char alphabet[26];
+
+
+
+//2. What is the size in memory of the following arrays
+//a. 
+float heights[5];
+//20 btyes
+
+//b. 
+int ages[10];
+//40 bytes
+
+//c. 
+char letters[26];
+//26 bytes
+
+
+
+//3. Declare an array with the values 1 - 10;
+int numbers[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+
+
+//4. Using the array created in problem 3 print the 5th index.
+
+
+
+//5. Using a loop, populate an array of size 10 with the values 10 - 1.
+//6. Create a an array of size five then ask the user to input five 
+//number into the array. Display the largest and smallest numbers 
+//to the console.
+//7. Create a program that creates a small 3x3 array of integers.Use a nested for 
+//loop to initialise the numbers 1 - 9. Output the numbers in a grid format.
