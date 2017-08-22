f7303abdc5bfef6fa25ee9cf7e9175be4e99e9cc
BrandonMartel <martelbrandon99@gmail.com>
2017-08-22 16:16:20 -0500

    arrays

--- Arrays/Arrays/Source.cpp
+++ Arrays/Arrays/Source.cpp
@@ -1,47 +1,62 @@
-//1. Declare an array for each of the following(size of array does not matter unless
-//sepcified).
-//a. heights of students
-//int studentHeight[60];
+#include "iostream";
 
-//b. age of parents
-//int parentAge[45];
+int main()
 
-//c. true of false questions
-// bool trueFalseQuestions[5];
+{
+	//1. Declare an array for each of the following(size of array does not matter unless
+	//sepcified).
+	//a. heights of students
+	//int studentHeight[60];
 
-//d. letters of the alphabet
-//char alphabet[26];
+	//b. age of parents
+	//int parentAge[45];
 
+	//c. true of false questions
+	// bool trueFalseQuestions[5];
 
-
-//2. What is the size in memory of the following arrays
-//a. 
-//float heights[5];
-//20 btyes
-
-//b. 
-//int ages[10];
-//40 bytes
-
-//c. 
-//char letters[26];
-//26 bytes
+	//d. letters of the alphabet
+	//char alphabet[26];
 
 
 
-//3. Declare an array with the values 1 - 10;
-//int numbers[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+	//2. What is the size in memory of the following arrays
+	//a. 
+	//float heights[5];
+	//20 btyes
+
+	//b. 
+	//int ages[10];
+	//40 bytes
+
+	//c. 
+	//char letters[26];
+	//26 bytes
 
 
 
-//4. Using the array created in problem 3 print the 5th index.
-int numbers[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-std::cout<<numbers
+
+	//3. Declare an array with the values 1 - 10;
+	//int numbers[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
 
-//5. Using a loop, populate an array of size 10 with the values 10 - 1.
-//6. Create a an array of size five then ask the user to input five 
-//number into the array. Display the largest and smallest numbers 
-//to the console.
-//7. Create a program that creates a small 3x3 array of integers.Use a nested for 
-//loop to initialise the numbers 1 - 9. Output the numbers in a grid format.
+
+
+	//4. Using the array created in problem 3 print the 5th index.
+	//int numbers[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+	//std::cout << numbers[5] << endl;
+
+
+
+
+	//5. Using a loop, populate an array of size 10 with the values 10 - 1.
+
+
+
+
+	//6. Create a an array of size five then ask the user to input five 
+	//number into the array. Display the largest and smallest numbers 
+	//to the console.
+	//7. Create a program that creates a small 3x3 array of integers.Use a nested for 
+	//loop to initialise the numbers 1 - 9. Output the numbers in a grid format.
+}
