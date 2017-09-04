fac9d5c16555e5581c6f0ca4db6e44ae88f981d9
BrandonMartel <martelbrandon99@gmail.com>
2017-09-03 21:39:00 -0500

    update

--- Functions/Functions/Source.cpp
+++ Functions/Functions/Source.cpp
@@ -15,5 +15,18 @@ int main()
 
 void numberswap()
 {
+	int a;
+	int b;
 
+	cout << "Enter two numbers to be swapped" << endl;
+	cin >> a;
+	cin >> b;
+
+	a = a + b;
+	b = a - b;
+	a = a - b;
+
+	cout << "The value of a is now " << a << endl;
+	cout << "The value of b is now " << b << endl;
+	return 0;
 }
