772d5414d495d200788ab5d892ccd84e9b962aaa
BrandonMartel <martelbrandon99@gmail.com>
2017-09-05 12:41:56 -0500

    update

--- Class Examples/Class Examples/Source.cpp
+++ Class Examples/Class Examples/Source.cpp
@@ -1,2 +1,33 @@
 #include <iostream>
 
+int AddTwoNumbers(int a, int b)
+{
+	int sum = a + b;
+	return sum;
+}
+
+
+
+
+int SubtractThreeNumbers(int a, int b, int c)
+{
+	int remainder = a - b - c;
+	return remainder;
+}
+
+
+
+int MultiplyByTen(int a)
+{
+	int product = a * 10;
+	return product;
+}
+
+
+
+
+float Halfer(float a)
+{
+	float half = a / 2;
+	return half;
+}
