ebb97cccce897452639d6a6b0db5c1dcd3ded303
BrandonMartel <martelbrandon99@gmail.com>
2017-09-06 11:39:46 -0500

    tt

--- Tic Tac Toe/Tic Tac Toe/Source.cpp
+++ Tic Tac Toe/Tic Tac Toe/Source.cpp
@@ -1,2 +1,16 @@
 #include <iostream>
 
+//Create an application that allows to users to play Tic - Tac - Toe.The game
+//should be able to played until the users decide to quit by either hitting the X button on
+//the window or they should have an on screen prompt in your application that pops up after
+//each game.If the user does not exit the game they must have an option to restart the game without
+//having to relaunch the application.You must provide a document breaking down the steps taken to
+//solve the problem.
+
+int main()
+{
+
+
+
+
+}
